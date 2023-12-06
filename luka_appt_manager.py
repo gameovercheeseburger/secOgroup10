a0bf4e7a54b4710449d797bda2bd6c05c18ef790
Luka-Haasdyk <152216140+Luka-Haasdyk@users.noreply.github.com>
2023-12-06 11:26:43 -0700

    Create luka_appt_manager.py
    started method

---
+++ luka_appt_manager.py
@@ -0,0 +1,8 @@
+def create_weekly_calendar():
+  
+  calendar = []
+  days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  hours_available = list(range(9, 17))
+
+  
+
