539953137d81e70f912b91f369a4915ccb31a227
Luka Haasdyk <luka.haasdyk@edu.sait.ca>
2023-12-06 12:22:06 -0700

    Signed-off-by: Luka Haasdyk <luka.haasdyk@edu.sait.ca>

---
+++ luka_appt_class.py
@@ -0,0 +1,43 @@
+class Appointment:
+    def __init__(self, day_of_week, start_time_hour):
+        self.client_name = ""
+        self.client_phone = ""
+        self.appt_type = 0
+        self.day_of_week = day_of_week
+        self.start_time_hour = start_time_hour
+
+    def get_client_name(self):
+        return self.client_name
+
+    def set_client_name(self, client_name):
+        self.client_name = client_name
+
+    def get_client_phone(self):
+        return self.client_phone
+
+    def set_client_phone(self, client_phone):
+        self.client_phone = client_phone
+
+
+    def get_day_of_week(self):
+        return self.day_of_week
+
+    def set_day_of_week(self, day_of_week):
+        self.day_of_week = day_of_week
+    
+    def get_start_time_hour(self):
+        return self.start_time_hour
+
+    def set_start_time_hour(self, start_time_hour):
+        self.start_time_hour = start_time_hour
+
+    
+    def get_appt_type_description():
+        
+
+
+
+
+
+  
+
