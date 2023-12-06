f27e2aafcfbfc7f8fd587d619ef37eb119f3b62d
Luka Haasdyk <luka.haasdyk@edu.sait.ca>
2023-12-06 13:06:32 -0700

    finished the appointment class

--- luka_appt_class.py
+++ luka_appt_class.py
@@ -9,29 +9,18 @@ class Appointment:
     def get_client_name(self):
         return self.client_name
 
-    def set_client_name(self, client_name):
-        self.client_name = client_name
-
     def get_client_phone(self):
         return self.client_phone
 
-    def set_client_phone(self, client_phone):
-        self.client_phone = client_phone
-
-
     def get_day_of_week(self):
         return self.day_of_week
-
-    def set_day_of_week(self, day_of_week):
-        self.day_of_week = day_of_week
     
     def get_start_time_hour(self):
         return self.start_time_hour
-
-    def set_start_time_hour(self, start_time_hour):
-        self.start_time_hour = start_time_hour
-
     
+    def get_appt_type(self):
+        return self.appt_type
+
     def get_appt_type_description(self):
         if self.appt_type == 0:
             return "Available"
@@ -45,6 +34,36 @@ class Appointment:
             return "Ladies Colouring"
         else:
             return "Invalid appt_type"
+        
+    def get_end_time_hour(self):
+        return self.start_time_hour + 1
+    
+    def set_client_name(self, client_name):
+        self.client_name = client_name
+
+    def set_client_phone(self, client_phone):
+        self.client_phone = client_phone
+
+    def set_appt_type(self, appt_type):
+        self.appt_type = appt_type
+
+    def schedule(self, client_name, client_phone, appt_type):
+        self.client_name = client_name
+        self.client_phone = client_phone
+        self.appt_type = appt_type
+
+    def cancel(self):
+        self.client_name = ""
+        self.client_phone = ""
+        self.appt_type = 0
+
+    def format_record(self):
+        return f"{self.client_name},{self.client_phone},{self.appt_type},{self.day_of_week},{self.start_time_hour}"
+
+    def __str__(self):
+        return f"{self.client_name}\t{self.client_phone} {self.day_of_week} {self.start_time_hour}:00 - {self.get_end_time_hour()}:00 {self.get_appt_type_desc()}"
+        
+    
 
 
 
