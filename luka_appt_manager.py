0a501987e6534868a8e9a832ea7d26e84ff954b3
Luka Haasdyk <luka.haasdyk@edu.sait.ca>
2023-12-06 12:22:06 -0700

    Signed-off-by: Luka Haasdyk <luka.haasdyk@edu.sait.ca>

--- luka_appt_manager.py
+++
@@ -1,9 +0,0 @@
-def create_weekly_calendar():
-  
-  calendar = []
-  days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-  hours_available = list(range(9, 17))
-  you
-
-  
-
