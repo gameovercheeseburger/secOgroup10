581ca6dd7ac15965a5ec274382819656e41c9e47
Patrik <144269021+gameovercheeseburger@users.noreply.github.com>
2023-12-10 00:48:24 -0700

    Update appointment.py
    Signed-off-by: Patrik  <144269021+gameovercheeseburger@users.noreply.github.com>

--- appointment.py
+++ appointment.py
@@ -1,8 +1,8 @@
 class Appointment:
-    def __init__(self, day_of_week, start_time_hour):
-        self.client_name = ""
-        self.client_phone = ""
-        self.appt_type = 0
+    def __init__(self, day_of_week, start_time_hour, client_name, client_phone, appt_type):
+        self.client_name = client_name
+        self.client_phone = client_phone
+        self.appt_type = appt_type
         self.day_of_week = day_of_week
         self.start_time_hour = start_time_hour
 
@@ -60,17 +60,8 @@ class Appointment:
     def format_record(self):
         return f"{self.client_name},{self.client_phone},{self.appt_type},{self.day_of_week},{self.start_time_hour}"
 
-    def __str__(self):
-        return f"{self.client_name:<15} {self.client_phone:>16} {self.day_of_week:>10} {'{:02d}'.format(self.start_time_hour):>3}:00 {'-':>2} {self.get_end_time_hour():>3}:00 {'':>3} {self.get_appt_type_description()}"
-
-
-def create_weekly_calendar():
-    calendar = []
-    days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    for day in days_of_week:
-        for hour in range(9, 17):
-            calendar.append(Appointment(day, hour))
-    return calendar
+def __str__(self):
+        return f"{self.client_name:<20} {self.client_phone:<15} {self.day_of_week:<15} {'{:02d}'.format(self.start_time_hour):>3}:00 {'-':>2} {self.get_end_time_hour():>3}:00 {'':>3} {self.get_appt_type_description():<20}"
 
 
       
